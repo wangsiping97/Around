f0fed5f74c1d5116227ac1be07e1a42637cc21dc
wangsiping97 <wangsiping1997@gmail.com>
2020-06-24 03:03:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
+	"strconv"
+	"context"
+
+	"github.com/olivere/elastic"
+)
+
+const (
+	POST_INDEX = "post"
+	DISTANCE = "200km"
+	ES_URL= "http://35.232.174.70:9200"
 )
 
 type Location struct {
@@ -24,6 +35,7 @@ type Post struct {
 func main() {
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -36,5 +48,65 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search") 
+
+	w.Header().Set("Content-Type", "application/json")
+
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+
+	fmt.Println("range is ", ran)
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon) 
+	searchResult, err := readFromES(query, POST_INDEX) 
+	if err != nil {
+		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
+		return
+	}
+	posts := getPostFromSearchResult(searchResult)
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+			http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+			fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+			return
+	}
+	w.Write(js)
+}
+
+func readFromES (query elastic.Query, index string) (*elastic.SearchResult, error) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL))
+	if err != nil {
+		return nil, err
+	}
+
+	searchResult, err := client.Search().
+		Index(index).Query(query).Pretty(true).Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResult, nil
+}
+
+func getPostFromSearchResult(searchResult *elastic.SearchResult) []Post {
+	var ptype Post
+	var posts []Post 
+
+	for _, item := range searchResult.Each(reflect.TypeOf(ptype)) {
+		p := item.(Post)
+		posts = append(posts, p)
+	}
+	return posts 
 }
 
