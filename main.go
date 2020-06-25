e267a9558acf16cf82a46bd22b0e50df5f9ce4a3
wangsiping97 <wangsiping1997@gmail.com>
2020-06-25 11:22:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,14 +8,32 @@ import (
 	"reflect"
 	"strconv"
 	"context"
+	"path/filepath"
+	"io"
 
 	"github.com/olivere/elastic"
+	"cloud.google.com/go/storage"
+	"github.com/pborman/uuid"
 )
 
 const (
 	POST_INDEX = "post"
 	DISTANCE = "200km"
 	ES_URL= "http://35.232.174.70:9200"
+	BUCKET_NAME = "around-sean-bucket"
+)
+
+var (
+	mediaTypes = map[string]string {
+		".jpeg": "image",
+		".jpg": "image",
+		".gif": "image",
+		".png": "image",
+		".mov": "video",
+		".avi": "video",
+		".flv": "video",
+		".wmv": "video",
+	}
 )
 
 type Location struct {
@@ -41,13 +59,50 @@ func main() {
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
+	w.Header().Set("Content-Type", "application/json")
+
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	
+	p := &Post {
+		User: r.FormValue("user"),
+		Message: r.FormValue("message"), 
+		Location: Location {
+			Lat: lat,
+			Lon: lon,
+		},
+		Face: 0.0, 
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	file, header, err := r.FormFile("image") 
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusBadRequest) 
+		fmt.Printf("Image is not available %v\n", err) 
+		return 
+	}
+
+	suffix := filepath.Ext(header.Filename) 
+	if t, ok := mediaTypes[suffix]; ok {
+		p.Type = t 
+	} else {
+		p.Type = "unknown" 
+	}
+
+	id := uuid.New() 
+	attrs, err := saveToGCS(file, id) 
+	if err != nil {
+		http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError) 
+		fmt.Printf("Failed to save image to GCS %v\n", err) 
+		return 
+	}
+	p.Url = attrs.MediaLink 
+
+	err = saveToES(p, POST_INDEX, id) 
+	if err != nil {
+		http.Error(w, "Failed to save post to Elasticsearch", http.StatusInternalServerError) 
+		fmt.Printf("Failed to save post to Elasticsearch %v\n", err) 
+		return 
+	}
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -103,10 +158,65 @@ func getPostFromSearchResult(searchResult *elastic.SearchResult) []Post {
 	var ptype Post
 	var posts []Post 
 
-	for _, item := range searchResult.Each(reflect.TypeOf(ptype)) {
+	for _, item := range searchResult.Each(reflect.TypeOf(ptype)) { // index & index 对应的 value
 		p := item.(Post)
 		posts = append(posts, p)
 	}
 	return posts 
 }
+
+func saveToGCS(r io.Reader, objectName string) (*storage.ObjectAttrs, error) {
+	ctx := context.Background() 
+
+	client, err := storage.NewClient(ctx) 
+	if err != nil {
+		return nil, err 
+	}
+
+	bucket := client.Bucket(BUCKET_NAME) 
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, err 
+	}
+
+	object := bucket.Object(objectName) 
+	wc := object.NewWriter(ctx) 
+	if _, err := io.Copy(wc, r); err != nil {
+		return nil, err 
+	}
+
+	if err := wc.Close(); err != nil {
+		return nil, err 
+	}
+
+	if err := object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, err 
+	}
+
+	attrs, err := object.Attrs(ctx) 
+	if err != nil {
+		return nil, err 
+	}
+
+	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink) 
+	return attrs, nil 
+}
+
+func saveToES(post *Post, index string, id string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL)) 
+	if err != nil {
+		return err 
+	}
+
+	_, err = client.Index().
+		Index(index).
+		Id(id).
+		BodyJson(post).
+		Do(context.Background())
+	if err != nil {
+		return err 
+	}
+
+	fmt.Printf("Post is saved to index: %s\n", post.Message) 
+	return nil 
+} 
 
